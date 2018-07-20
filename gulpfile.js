5164185858d2516955a16d321112a9733d0eae88
MrTiZel <worldface.volodin@yandex.ru>
2018-07-20 22:58:42 +0300

    разметка выполнена

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('scss', function () {
         .pipe(gulpIf(config.isDevelop, sourcemaps.init()))
         .pipe(sass())
         .pipe(concat(config.output.cssName))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({browsers: ['last 2 version', 'safari 5']}))
         .pipe(gulpIf(!config.isDevelop, cleanCss()))
         .pipe(gulpIf(config.isDevelop, sourcemaps.write()))
         .pipe(gulp.dest(config.output.path))
